f5a9f2d01cb4195ec6eab8aa0b4f5debf9c422c4
Remko Deenik <remko@continuance.nl>
2020-05-08 16:34:51 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,11 +6,11 @@
 # Generate a public/private key for passwordless authentication
 resource "null_resource" "generate-sshkey" {
     provisioner "local-exec" {
-        command = "mkdir -p dirname(${var.k8s-global.private_key})"
+        command = "mkdir -p ${dirname(var.k8s-global.private_key)}"
     }
     provisioner "local-exec" {
-        # TODO: only run if keys do not exist
         command = "yes n | ssh-keygen -b 4096 -t rsa -C 'k8s-on-vmware-sshkey' -N '' -f ${var.k8s-global.private_key}"
+        on_failure = "continue"
     }
 }
 
